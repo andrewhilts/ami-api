91386d308c77b60f133a56482638b0f594e25d0b
Andrew Hilts <andrewchilts@gmail.com>
2016-05-17 17:31:12 -0400

    canada mods

--- ami-api.php
+++ ami-api.php
@@ -7,7 +7,9 @@ function ami_api_init( $server ) {
 
 	require_once dirname( __FILE__ ) . '/class-ami-api-mytype.php';
 	$ami_api = new Ami_Api( $server );
-	add_filter( 'json_endpoints', array( $ami_api, 'register_routes' ) );
+	add_filter('json_endpoints', array( $ami_api, 'register_routes' ) );
+	// Remove JSON API CORS HEADERS; manage in-server
+	remove_filter( 'json_serve_request', 'json_send_cors_headers'             );
 	// $myplugin->register_filters();
 }
 function add_query_vars_filter( $vars ){
@@ -16,8 +18,6 @@ function add_query_vars_filter( $vars ){
   return $vars;
 }
 
-// Remove JSON API CORS HEADERS; manage in-server
-// remove_filter( 'json_serve_request', 'json_send_cors_headers'             );
 
 add_filter( 'query_vars', 'add_query_vars_filter' );
 add_action( 'wp_json_server_before_serve', 'ami_api_init' , 100);
